fb308f254b308ad024a8c864668e7f36decc7119
MCDeltaT <68516357+MustardChef@users.noreply.github.com>
2023-09-01 09:05:44 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -189,6 +189,8 @@ CUSTOM_MODEL_MAP=(
     "bluejay"
     "panther"
     "cheetah"
+    "lynx"
+    "tangorpro"
 )
 
 ROOT_SOL_MAP=(
@@ -286,13 +288,10 @@ GAPPS_PATH=$DOWNLOAD_DIR/$GAPPS_ZIP_NAME
 WSA_MAJOR_VER=0
 
 update_ksu_zip_name() {
-    KERNEL_VER="5.15.94.4"
-    if [ "$WSA_MAJOR_VER"="2306" ]; then
-        KERNEL_VER="5.15.104.1"
-    fi    
-    if [ "$WSA_MAJOR_VER"="2307" ]; then
-        KERNEL_VER="5.15.104.2"
-    fi
+    KERNEL_VER="5.15.104.2"
+    #if [ "$WSA_MAJOR_VER" -ge "2307" ]; then
+    #    KERNEL_VER="5.15.104.3"
+    #fi
     KERNELSU_ZIP_NAME=kernelsu-$ARCH-$KERNEL_VER.zip
     KERNELSU_PATH=$DOWNLOAD_DIR/$KERNELSU_ZIP_NAME
     KERNELSU_APK_PATH=$DOWNLOAD_DIR/KernelSU.apk
@@ -627,7 +626,7 @@ fi
 EOF
     # Grant access
     sudo chmod 0755 "$KSU_PRE"
-    sudo chown root:root "$KSU_PRE"
+    sudo chown 0:2000 "$KSU_PRE"
     sudo setfattr -n security.selinux -v "u:object_r:system_file:s0" "$KSU_PRE" || abort
     echo -e "Add KernelSU Manager done\n"
 fi
@@ -685,17 +684,17 @@ fi
 if [[ "$CUSTOM_MODEL" != "none" ]]; then
     echo "Fix system props"
     # The first argument is prop path, second is product name (redfin), third is device model (Pixel 5)
-    declare -A MODEL_NAME_MAP=(["sunfish"]="Pixel 4a" ["bramble"]="Pixel 4a (5G)" ["redfin"]="Pixel 5" ["barbet"]="Pixel 5a" ["raven"]="Pixel 6 Pro" ["oriole"]="Pixel 6" ["bluejay"]="Pixel 6a" ["panther"]="Pixel 7" ["cheetah"]="Pixel 7 Pro")
+    declare -A MODEL_NAME_MAP=(["sunfish"]="Pixel 4a" ["bramble"]="Pixel 4a (5G)" ["redfin"]="Pixel 5" ["barbet"]="Pixel 5a" ["raven"]="Pixel 6 Pro" ["oriole"]="Pixel 6" ["bluejay"]="Pixel 6a" ["panther"]="Pixel 7" ["cheetah"]="Pixel 7 Pro" ["lynx"]="Pixel 7a" ["tangorpro"]="Pixel Tablet")
     MODEL_NAME="${MODEL_NAME_MAP[$CUSTOM_MODEL]}"
     sudo python3 fixGappsProp.py "$ROOT_MNT" "$CUSTOM_MODEL" "$MODEL_NAME" || abort
     # shellcheck disable=SC2002
-    BUILD_ID=$(sudo cat "$SYSTEM_MNT/build.prop" | grep -e ro.build.id | cut -c 13-)
-    if [[ "${#BUILD_ID}" != "15" ]]; then
-        FIXED_BUILD_ID=$(echo "$BUILD_ID" | cut -c -15)
-        echo "Fixed Build ID: $FIXED_BUILD_ID"
-        # shellcheck disable=SC2086
-        sudo find $ROOT_MNT/{system,system_ext,product,vendor} -name "build.prop" -exec sed -i -e "s/$BUILD_ID/$FIXED_BUILD_ID/g" {} \;
-    fi
+    #BUILD_ID=$(sudo cat "$SYSTEM_MNT/build.prop" | grep -e ro.build.id | cut -c 13-)
+    #if [[ "${#BUILD_ID}" != "15" ]]; then
+    #    FIXED_BUILD_ID=$(echo "$BUILD_ID" | cut -c -15)
+    #    echo "Fixed Build ID: $FIXED_BUILD_ID"
+    #    # shellcheck disable=SC2086
+    #    sudo find $ROOT_MNT/{system,system_ext,product,vendor} -name "build.prop" -exec sed -i -e "s/$BUILD_ID/$FIXED_BUILD_ID/g" {} \;
+    #fi
     echo -e "done\n"
     MODEL_NAME="${MODEL_NAME// /-}"
 else
